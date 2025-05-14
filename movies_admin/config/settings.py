ff436bf0e279f180864c30f8a1d1f16028c34610
ichmrklv <merrkl@mail.ru>
2025-05-14 22:13:10 +0300

    Moved DEBUG setting to environment variables

--- movies_admin/config/settings.py
+++ movies_admin/config/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"] 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True # os.environ.get('DEBUG', False) == 'True'
+DEBUG = os.environ.get('DEBUG', False) == 'True'
 
 ALLOWED_HOSTS = []
 
